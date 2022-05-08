793d35b5ccc7d5f40f04f50aa00fba7c8d886ecf
HTY <1044213317@qq.com>
2022-05-08 06:21:07 +0000

    softmax torch

---
+++ softmax/Pytorch/Pytorch/test.py
@@ -0,0 +1,7 @@
+from torch.utils.data import DataLoader
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
---
+++ softmax/Pytorch/Pytorch/utils/visualize.py
@@ -0,0 +1,6 @@
+class Visualize():
+    def __init__(self, model) -> None:
+        self.model = model
+
+    def show(self):
+        pass
